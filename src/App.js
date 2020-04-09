56f34feb1dad66686313cb7aafb42ec73b45cf4f
Andrew Cobb <andrew@cobbs-web.com>
2020-04-09 12:29:53 -0400

    heroku debug

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Container} from '@material-ui/core';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Title from './components/Title';
+import HomeComponent from './components/HomeComponent'
+import LoginComponent from './components/LoginComponent'
+import LogsComponent from './components/LogsComponent';
+import UsersComponent from './components/UsersComponent';
+import EditUserComponent from './components/EditUserComponent';
+import AddUserComponent from './components/AddUserComponent'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+    <Title />
+    <BrowserRouter>
+      <Route path="/" component={HomeComponent} exact></Route>
+      <Route path="/login" component={LoginComponent} exact></Route>
+      <Route path="/logs" component={LogsComponent} exact></Route>
+      <Route path="/users" component={UsersComponent} exact></Route>
+      <Route path="/edituser/:id" component={EditUserComponent} exact></Route>
+      <Route path="/adduser" component={AddUserComponent} exact></Route>
+    </BrowserRouter>
+  </Container>,
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Container} from '@material-ui/core';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Title from './components/Title';
-import HomeComponent from './components/HomeComponent'
-import LoginComponent from './components/LoginComponent'
-import LogsComponent from './components/LogsComponent';
-import UsersComponent from './components/UsersComponent';
-import EditUserComponent from './components/EditUserComponent';
-import AddUserComponent from './components/AddUserComponent'
+
 
 ReactDOM.render(
-  <Container>
-    <Title />
-    <BrowserRouter>
-      <Route path="/" component={HomeComponent} exact></Route>
-      <Route path="/login" component={LoginComponent} exact></Route>
-      <Route path="/logs" component={LogsComponent} exact></Route>
-      <Route path="/users" component={UsersComponent} exact></Route>
-      <Route path="/edituser/:id" component={EditUserComponent} exact></Route>
-      <Route path="/adduser" component={AddUserComponent} exact></Route>
-    </BrowserRouter>
-  </Container>,
+  <App />,
   document.getElementById('root')
 );
 
